ed9d3d215cdf6addae8b6b22d9413189dff075a9
Aleksandar Pavlovic <sweetsare42@gmail.com>
2023-05-21 15:26:37 +0200

    feat: Add cart icon and create routes for new pages
    This commit adds a cart icon to the Navbar component using an SVG image. The cart icon is imported and rendered in the Navbar component. Additionally, routes for new pages have been created, allowing navigation to different pages such as the cart page. This update enhances the navigation and functionality of the application.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import HomePage from './HomePage.js';
+import Shop from './Shop.js';
+import FrequentlyAskedQuestions from './FrequentlyAskedQuestions.js';
+import Cart from './Cart.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
 
 
 
 function App() {
   return (
-    <HomePage />
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/Shop" element={<Shop />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/FrequentlyAskedQuestions" element={<FrequentlyAskedQuestions />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/Cart" element={<Cart />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ReactComponent as Logo } from '/home/lucid/repos/premium-shop/src/images/logo.svg';
+import { ReactComponent as ShoppingBag} from '/home/lucid/repos/premium-shop/src/images/shoppingBag.svg';
 import './Navbar.css'; // Add the CSS file for Navbar styles
 import './fonts.css'; // Add the CSS file for fonts
 
@@ -13,6 +14,7 @@ function Navbar() {
         <a href="/">Home</a>
         <a href="/shop">Shop</a>
         <a href="/faq">FAQ</a>
+        <a href="/cart"><ShoppingBag className="cart-icon" /></a>
       </div>
     </div>
   );
